d4d5eca6dcdb15964c34c72936f68a63aa873eb8
adcpm <fabien@bonustrack.co>
2019-06-22 15:04:11 +0700

    Clean package with working example

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# libra
+[![Join the chat](https://img.shields.io/discord/590507340399116288.svg)](https://discord.gg/XyS25F6)
+
+# Libra gRPC JavaScript Client
+
+```
+# install dependencies
+npm install
+
+# make a basic request to a testnet node
+npm run start
+```
+
+### Get help
+
+* [Libra Developer Chat Telegram](https://t.me/joinchat/DAQb4RSNpqEok3p-QdmaKQ)
+* [Libra Developer Chat Discord](https://discord.gg/XyS25F6)
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+const path = require('path');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-const PROTO_PATH = __dirname + '/node_debug_interface.proto'; // Taken from https://github.com/libra/libra/blob/master/common/debug_interface/src/proto/node_debug_interface.proto
+const PROTO_PATH = path.resolve(__dirname, './proto/admission_control.proto');
+const SERVER_URI = 'ac.testnet.libra.org:8000';
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-const debugProto = grpc.loadPackageDefinition(packageDefinition).debug;
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 
-const client = new debugProto.NodeDebugInterface('ac.testnet.libra.org:8000', grpc.credentials.createInsecure());
+const client = new protoDescriptor.admission_control.AdmissionControl(SERVER_URI, grpc.credentials.createInsecure());
 
-client.getNodeDetails(null, function(err, response) {
-  console.log('Greeting:', err, response);
+client.updateToLatestLedger({
+  client_known_version: 0,
+}, (err, response) => {
+  console.log('Result', err, response);
 });
